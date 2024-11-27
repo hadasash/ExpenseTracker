85ae72b34e41b518692c8e59399a7eb16bd64c67
Personal <mijaluli@gmail.com>
2024-11-27 13:28:08 +0200

    translations

--- myapp/src/components/ExpenseManagement/ExpenseSummary.jsx
+++ myapp/src/components/ExpenseManagement/ExpenseSummary.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Typography,  Box  } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const ExpenseSummary = ({ selectedMonth, totalAmount, year }) => {
+  const { t } = useTranslation();
   const formattedAmount = totalAmount.toFixed(3);
 
   return (
     <>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
         <Typography variant="h6" color="textSecondary">
-          סיכום חודשי - {selectedMonth}/{year}
+        {t('monthlySummary')}- {selectedMonth}/{year}
         </Typography>
       </Box>
       <Typography variant="h4" sx={{ fontWeight: 'bold',  }}>
         ₪{formattedAmount}
       </Typography>
       <Typography variant="body1" color="textSecondary">
-        סה"כ הוצאות חודשיות
+        {t('monthlySummaryTitle')}
       </Typography>
     </>
   );

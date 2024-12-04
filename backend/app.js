37a2109bdc5921a8a55f0b28f630cdd1481fffb7
Personal <mijaluli@gmail.com>
2024-12-04 12:39:00 +0200

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');  // Ensure this is required
 const ExpensesRouter = require('./routes/expenses');
 const cors = require('cors'); 
 
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || '*',  // You can set this to a specific origin (e.g., 'https://yourfrontend.com')
+  origin: process.env.CORS_ORIGIN || '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
-app.use(express.static(path.join(__dirname, 'myapp/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'myapp/build', 'index.html'));
-});
-
 app.use(cors(corsOptions));
+app.use(express.json());
 
 async function connectToDatabase() {
   try {
@@ -37,12 +33,16 @@ async function connectToDatabase() {
   await connectToDatabase();
   console.log('Database connection established');
 
-  app.use(express.json());
   app.use('/expenses', ExpensesRouter);
+
+  app.use(express.static(path.join(__dirname, 'myapp/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'myapp/build', 'index.html'));
+  });
 
   const port = process.env.PORT || 3000;
 
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-  }); 
+  });
 })();

33f9e50e9451145ac53b6278cdb5cece43a3884c
Personal <mijaluli@gmail.com>
2024-12-15 12:25:50 +0200

    vite config

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import dotenv from 'dotenv';
-import react from '@vitejs/plugin-react'
-
-// Load environment variables from .env
-dotenv.config();
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env,
+    'process.env': {
+      VITE_API_URL: process.env.VITE_API_URL,
+    },
   },
 });
-

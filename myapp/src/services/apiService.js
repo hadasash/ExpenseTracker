49eb78a55bb34982f90a6adb1f89cfbf7214d059
Personal <mijaluli@gmail.com>
2024-12-15 12:49:30 +0200

    vite

--- myapp/src/services/apiService.js
+++ myapp/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 console.log('VITE_API_URL:', API_BASE_URL);
 
 export default  {

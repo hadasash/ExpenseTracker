68512555b5f2bc94de7afe4f1fb50d54aa528270
Personal <mijaluli@gmail.com>
2024-12-15 14:49:23 +0200

    vite

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {
-      VITE_API_URL: process.env.VITE_API_URL,
-    },
-  },
 });

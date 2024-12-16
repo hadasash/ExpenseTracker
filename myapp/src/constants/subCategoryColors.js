c8b4210c092671e9bcb305e268bb0596f330cfd9
hadasasharon@gmail.com <hadasasharon@gmail.com>
2024-12-16 20:51:40 +0200

    colors

--- myapp/src/constants/subCategoryColors.js
+++ myapp/src/constants/subCategoryColors.js
@@ -1,15 +1,28 @@
+// export const subCategoryColors = {
+//     salariesAndRelated: '#003366',
+//     commissions: '#336699',
+//     equipmentAndSoftware: '#6699CC',
+//     officeExpenses: '#990000',
+//     vehicleMaintenance: '#CC3333',
+//     depreciation: '#FF6666',
+//     managementServices: '#003300',
+//     professionalServices: '#FFCC00',
+//     advertising: '#660066',
+//     rentAndMaintenance: '#993399',
+//     postageAndCommunications: '#CC6600',
+//     officeAndOther: '#006666',
+//   };
 export const subCategoryColors = {
-    salariesAndRelated: '#003366',
-    commissions: '#336699',
-    equipmentAndSoftware: '#6699CC',
-    officeExpenses: '#990000',
-    vehicleMaintenance: '#CC3333',
-    depreciation: '#FF6666',
-    managementServices: '#003300',
-    professionalServices: '#FFCC00',
-    advertising: '#660066',
-    rentAndMaintenance: '#993399',
-    postageAndCommunications: '#CC6600',
-    officeAndOther: '#006666',
-  };
-  
+  salariesAndRelated: '#FF69B4',  // ורוד בוהק
+  commissions: '#00BFFF',         // כחול חשמלי
+  equipmentAndSoftware: '#32CD32',  // ירוק ליים
+  officeExpenses: '#FFD700',      // צהוב זהב
+  vehicleMaintenance: '#FF6347',  // אדום עז
+  depreciation: '#FF4500',        // כתום חזק
+  managementServices: '#8A2BE2',  // סגול כהה
+  professionalServices: '#FF1493', // ורוד חזק
+  advertising: '#FF6347',         // כתום אדום
+  rentAndMaintenance: '#00FF7F',  // ירוק טורקיז
+  postageAndCommunications: '#FF4500', // כתום חזק
+  officeAndOther: '#00CED1',      // טורקיז כהה
+};
